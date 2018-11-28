28488d6441e3dec8240f0aaac52aeaf1916ba940
Maayan <maayan.albert1@gmail.com>
2018-11-28 02:10:51 -0500

    cleaned up code

--- genderbender/views.py
+++ genderbender/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .genderBender import bend
 
-# Create your views here.
+# default view
 def default(request):
     return render(request, 'genderbender/index.html',{})
 
-
+# check if a string can be converted to an int
+# INPUT: the string in question
+# OUTPUT: the integer version of the string or a nonetype 
 def returnIntOrNone(string):
     try:
         string= int(string)
@@ -14,17 +16,26 @@ def returnIntOrNone(string):
     except ValueError:
         return None
 
+# genderbends the input
+# INPUT: the string to be converted and optionally the year it was written in
+# OUTPUT: the the converted text 
 def bendInput(request):
+
+    # get the string and year
     string = request.POST.get('string')
     year = returnIntOrNone(request.POST.get('year'))
+
+    # bend the text (with the year if its valid)
     if(year == None):
         bentString = bend(string)
     else:
         bentString = bend(string, year)
     context = dict()
 
+    # populate the response data
     context["bentString"] = bentString
     if(year != None):
         context["year"] = year
+        
     return render(request, 'genderbender/index.html',context)
 
